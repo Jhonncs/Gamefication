2c905ce66fbbb0267f3b44a8436226505fb822aa
AndersonGuimaraes53 <31110579+AndersonGuimaraes53@users.noreply.github.com>
2017-08-23 17:16:01 -0300

    Add files via upload

--- Gamification.Rest/Properties/AssemblyInfo.cs
+++ Gamification.Rest/Properties/AssemblyInfo.cs
@@ -2,34 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gamification.Rest")]
+[assembly: AssemblyTitle("Gamification")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gamification.Rest")]
+[assembly: AssemblyProduct("Gamification")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("65fe070d-04db-434a-aa31-8b16dc999a26")]
+[assembly: Guid("e5e9ef17-e8f9-4649-b8fd-176e0d701e2f")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Gamification.Rest/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using MongoDB.Helper.Repositorio;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Helper.Interfaces;
+using Gamification.DAL.Entidades;
+
+namespace Gamification.DAL.Repositorio
+{
+	public class UsuarioRepositorio : RepositorioBase<Usuario>
+	{
+		public List<Usuario> GetUsuarioPorNome(string nome)
+		{
+			return Collection.Find(usuarioDoBanco => usuarioDoBanco.Nome == nome).ToList();
+		}
+
+		public Usuario GetUsuarioPorLogin(string login)
+		{
+			return Collection.Find(usuarioDoBanco => usuarioDoBanco.Credencial.Login == login).FirstOrDefault();
+		}		
+
+	}
+}
