1161d95b00c755a8e634dabc8ac9694cc0e8077f
Jhonncs <Jhonncs@users.noreply.github.com>
2017-08-23 13:47:56 -0300

    Add files via upload

---
+++ Gamification.Rest/Usuario/UsuarioRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gamification.Rest.Usuarios
+{
+	public class UsuarioRequest
+	{
+
+		public string nome;
+		public int idade;
+		public string login;
+		public string senha;
+
+
+	}
+}
